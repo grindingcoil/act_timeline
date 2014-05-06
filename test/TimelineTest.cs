c61c0ba3032636efba1f89600f0eec3d236c5850
grindingcoil <anonymous>
2014-05-06 16:34:33 +0900

    Add before/after window test

---
+++ test/TimelineTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACTTimeline;
+
+namespace test
+{
+    [TestClass]
+    public class TimelineTest
+    {
+        [TestMethod]
+        public void AnchorWindowShouldBeRespected()
+        {
+            Timeline t = TimelineLoader.LoadFromText("test", "100 テスト sync /a/ window 20,30\n");
+            TimelineAnchor a = t.Anchors.First();
+
+            Assert.AreSame(a, t.FindAnchorMatchingLogline(100, "a"), "With in window");
+            Assert.AreSame(a, t.FindAnchorMatchingLogline(81, "a"), "With in before window");
+            Assert.AreSame(a, t.FindAnchorMatchingLogline(129, "a"), "With in after window");
+
+            Assert.IsNull(t.FindAnchorMatchingLogline(79, "a"), "Out of before window");
+            Assert.IsNull(t.FindAnchorMatchingLogline(131, "a"), "Out of after window");
+        }
+    }
+}
